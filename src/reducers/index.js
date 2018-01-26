8c1897750e4886e2023de5c842e5be270d6cb6d0
Anton Lehmus <anton.lehmus@gmail.com>
2018-01-26 14:55:30 +0200

    fix webpack config and add redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import {reducer as formReducer} from 'redux-form';
 import StationsReducer from './reducer_stations'
 
 const rootReducer = combineReducers({
-  stations: StationsReducer
+  stations: StationsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
