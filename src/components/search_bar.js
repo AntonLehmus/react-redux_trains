4f433dce2961ab9a8369a3b5d4f63337b0affd99
Anton Lehmus <anton.lehmus@gmail.com>
2018-01-26 14:37:31 +0200

    fix search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,21 +4,28 @@ import Autosuggest from 'react-autosuggest';
 
 import {fetchStations} from '../actions';
 
-const stations = this.props.stations;
+class SearchBar extends Component {
 
-render() {
+    componentDidMount() {
+        this.props.fetchStations();
+    }
+
+
+  render() {
     return (
-      <div className="input-group">
-        <input
-            type="text" className="form-control" placeholder="Search for station"
-            id="search_bar"
-        />
-        <span className="input-group-btn">
-            <button className="btn" type="button" id="search_submit"><i className="fa fa-search "></i> Go!</button>
-        </span>
-      </div>
+        <form>
+            <div className="input-group">
+                <input
+                    type="text" className="form-control" placeholder="Search for station"
+                    id="search_bar"
+                />
+                <span className="input-group-btn">
+                    <button className="btn" type="submit" id="search_submit"><i className="fa fa-search "></i> Go!</button>
+                </span>
+            </div>
+      </form>
     );
-}
+  }
 
 }
 
